003fa36059178eaa7c7854cd20308820506139bf
BeatrizMaryah <beatriz.carmo@alunos.sc.senac.br>
2021-10-16 20:39:15 -0300

    Mudanças no css.

--- src/main/java/modelo/entidade/estudantil/Escola.java
+++ src/main/java/modelo/entidade/estudantil/Escola.java
@@ -35,7 +35,7 @@ public class Escola extends Usuario implements Serializable {
 	}
 	
 	public Escola(Long id, String nome, Contato contato, String login, String senha) {
-		super(id, nome, login, senha, contato);
+		super(id, nome, login, senha);
 	}
 	
 	public Escola(Long id, String nome, String login, String senha) {
